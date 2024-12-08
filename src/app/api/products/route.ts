ba0917218d4b54b33f9074e4b09abbbad9ea0c2c
Izma Rao <147949671+IzmaRao@users.noreply.github.com>
2024-12-08 20:15:46 +0500

    Update route.ts

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 
-export function GET(request: NextRequest) {
+export function GET() {
     return NextResponse.json([
         { id: 1, name: "Trenton modular sofa_3", price: "25,000.00", image: "/Trenton modular sofa_3 1.png", width: 283, height: 283 },
         { id: 2, name: "Granite dining table with dining chair", price: "25,000.00", image: "/Granite dining table with dining chair 1.png", width: 283, height: 283 },
